74728ca35138d0fdc2dd695de2f59989b35c15f6
nicho92 <nicolas.pihen@gmail.com>
2021-07-09 19:33:09 +0200

    update version

--- src/main/java/org/api/mtgstock/tools/MTGStockConstants.java
+++ src/main/java/org/api/mtgstock/tools/MTGStockConstants.java
@@ -17,7 +17,7 @@ public class MTGStockConstants {
 	public static final String FULL_ART="- Full Art";
 	public static final String ETCHED="Foil Etched";
 	public static final String JAPANESE="JP Alternate Art";
-	public static final String VERSION = "0.1.12";
+	public static final String VERSION = "0.1.16";
 	
 	
 	public enum PRICES {LOW, AVG, HIGH, FOIL, MARKET, MARKET_FOIL,AVERAGE}
